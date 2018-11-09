b24d8dda05777cccce70c7ab77e110e1a3be416f
Aedirn <byroninjeeli@gmail.com>
2018-11-09 16:31:34 -0500

    commit

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCTnyV6vlg2vakA3hGE0YNr4vIh5aWoWhA",
-    authDomain: "users-f89df.firebaseapp.com",
-    databaseURL: "https://users-f89df.firebaseio.com",
-    projectId: "users-f89df",
-    storageBucket: "users-f89df.appspot.com",
-    messagingSenderId: "329236793287"
-  };
-  firebase.initializeApp(config);
-
-  const db = firebase.database();
-
-// FirebaseUI config.
-var uiConfig = {
-    signInSuccessUrl: `https://byronthe123.github.io/users/main.html`,
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ],
-    'callbacks': {
-        'signInSuccess': function(currentUser, credential, redirectUrl) {
-            window.location.assign('https://byronthe123.github.io/users/main.html');
-            authStateLogin();
-            return true;
-        }
-    }
-};
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
 const authStateLogin = () => {
     console.log('login');
     $('#log_status').text('logged in');
@@ -69,6 +34,42 @@ const authStateLogin = () => {
         }
     });*/
 }
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCTnyV6vlg2vakA3hGE0YNr4vIh5aWoWhA",
+    authDomain: "users-f89df.firebaseapp.com",
+    databaseURL: "https://users-f89df.firebaseio.com",
+    projectId: "users-f89df",
+    storageBucket: "users-f89df.appspot.com",
+    messagingSenderId: "329236793287"
+  };
+  firebase.initializeApp(config);
+
+  const db = firebase.database();
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: `https://byronthe123.github.io/users/main.html`,
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+    'callbacks': {
+        'signInSuccess': function(currentUser, credential, redirectUrl) {
+            window.location.assign('https://byronthe123.github.io/users/main.html');
+            authStateLogin();
+            return true;
+        }
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
 
 
 // db.ref().on('value', (snapshot) => {
