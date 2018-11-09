841787bdfeba805e7f9d2f3a07f9b2ab799daef7
Aedirn <byroninjeeli@gmail.com>
2018-11-09 16:57:26 -0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,68 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCTnyV6vlg2vakA3hGE0YNr4vIh5aWoWhA",
+    authDomain: "users-f89df.firebaseapp.com",
+    databaseURL: "https://users-f89df.firebaseio.com",
+    projectId: "users-f89df",
+    storageBucket: "users-f89df.appspot.com",
+    messagingSenderId: "329236793287"
+  };
+  firebase.initializeApp(config);
+
+  const db = firebase.database();
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: `https://byronthe123.github.io/users/main.html`,
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          // User successfully signed in.
+          authStateLogin();
+          window.location.href = `https://byronthe123.github.io/users/main.html`;
+          // don't redirect automatically
+          return false;
+        }
+    },
+};
+
 const authStateLogin = () => {
-    console.log('login');
+    console.log('logged in');
     $('#log_status').text('logged in');
-    /*firebase.auth().onAuthStateChanged((user) => {
+
+    console.log(db.ref());
+}
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+// db.ref().on('value', (snapshot) => {
+//     console.log(snapshot);
+//     console.log(snapshot.hasOwnProperty('node_.children_'));
+//     console.log(snapshot.node_.children_.root_.left);
+//     if(snapshot.node_.children_.root_.left === undefined) {
+//         console.log('null');
+//     }
+//     // console.log(snapshot.node_.children_.root_.left.value.value_);
+//     // if(snapshot.node_.children_.root_.left.value.value_ !== email) {
+//     //     db.ref().push({
+//     //         db_username: username,
+//     //         db_email: email
+//     //     });
+//     // }
+// });
+
+
+/* 
+    firebase.auth().onAuthStateChanged((user) => {
         console.log(db.ref());
         if(!user) {
             // if(window.location.href === 'https://byronthe123.github.io/random_12347/main.html') {
@@ -32,61 +93,5 @@ const authStateLogin = () => {
 
             });
         }
-    });*/
-}
-  
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCTnyV6vlg2vakA3hGE0YNr4vIh5aWoWhA",
-    authDomain: "users-f89df.firebaseapp.com",
-    databaseURL: "https://users-f89df.firebaseio.com",
-    projectId: "users-f89df",
-    storageBucket: "users-f89df.appspot.com",
-    messagingSenderId: "329236793287"
-  };
-  firebase.initializeApp(config);
-
-  const db = firebase.database();
-
-// FirebaseUI config.
-var uiConfig = {
-    signInSuccessUrl: `https://byronthe123.github.io/users/main.html`,
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-          // User successfully signed in.
-          authStateLogin();
-          console.log("sign in success");
-          window.location.href = `https://byronthe123.github.io/users/main.html`
-          // don't redirect automatically
-          return false;
-        }
-    },
-};
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
-
-
-// db.ref().on('value', (snapshot) => {
-//     console.log(snapshot);
-//     console.log(snapshot.hasOwnProperty('node_.children_'));
-//     console.log(snapshot.node_.children_.root_.left);
-//     if(snapshot.node_.children_.root_.left === undefined) {
-//         console.log('null');
-//     }
-//     // console.log(snapshot.node_.children_.root_.left.value.value_);
-//     // if(snapshot.node_.children_.root_.left.value.value_ !== email) {
-//     //     db.ref().push({
-//     //         db_username: username,
-//     //         db_email: email
-//     //     });
-//     // }
-// });
+    });
+*/
